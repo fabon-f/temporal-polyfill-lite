20fff97980d9d43cfbccb76a340b7af521701ad9
fabon <syobon.hinata.public@gmail.com>
2025-12-13 20:07:10 +0900

    Mangle internal properties for build

---
+++ script/shared/build.ts
@@ -0,0 +1,23 @@
+import terser from "@rollup/plugin-terser";
+import { rolldown } from "rolldown";
+
+// @ts-expect-error https://github.com/rollup/plugins/issues/1860
+const terserPlugin = terser({
+	mangle: {
+		properties: {
+			regex: /^(_|\$)/,
+		},
+	},
+});
+
+export async function bundle() {
+	await using bundle = await rolldown({
+		input: "src/global.ts",
+		plugins: [terserPlugin],
+	});
+	const result = await bundle.generate({
+		format: "esm",
+		plugins: [terserPlugin],
+	});
+	return result.output[0].code;
+}
