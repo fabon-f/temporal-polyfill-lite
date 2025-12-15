952a62cf9df6f8e3dec67ad67ff11b80c7d1772e
fabon <syobon.hinata.public@gmail.com>
2025-12-15 13:53:47 +0900

    Implement `Temporal.Now.timeZoneId`

--- src/Now.ts
+++ src/Now.ts
@@ -1,7 +1,19 @@
+import { OriginalDateTimeFormat } from "./DateTimeFormat.ts";
 import { defineStringTag, makePropertiesNonEnumerable } from "./internal/property.ts";
 
+/** `SystemTimeZoneIdentifier` */
+function systemTimeZoneIdentifier() {
+	// Creating `Intl.DateTimeFormat` is a CPU-heavy operation in many JS engines,
+	// but caching it can cause an unexpected behavior for polyfill users, because the polyfill can return an outdated value.
+	// Unfortunately there's no way to observe a system time zone change effectively.
+	// cf. https://github.com/fullcalendar/temporal-polyfill/issues/63
+	return new OriginalDateTimeFormat().resolvedOptions().timeZone;
+}
+
 export const Now = {
-	timeZoneId() {},
+	timeZoneId() {
+		return systemTimeZoneIdentifier();
+	},
 	instant() {},
 	plainDateTimeISO() {},
 	zonedDateTimeISO() {},
