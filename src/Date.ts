71b71e120972fa6eb8ae78b136bde85d23485eba
fabon <syobon.hinata.public@gmail.com>
2026-01-08 07:22:49 +0900

    Make `Date.p.toTemporalInstant` not work as constructor

--- src/Date.ts
+++ src/Date.ts
@@ -1,11 +1,16 @@
 import { createTemporalInstant, type Instant } from "./Instant.ts";
 import { createEpochNanosecondsFromEpochMilliseconds } from "./internal/epochNanoseconds.ts";
 
-export function toTemporalInstant(this: unknown): Instant {
-	// brand check
-	const epochMilliseconds = Date.prototype.valueOf.call(this);
-	if (isNaN(epochMilliseconds)) {
-		throw new RangeError();
+class TmpClass {
+	// make `toTemporalInstant` not work as constructor
+	toTemporalInstant(this: unknown): Instant {
+		// brand check
+		const epochMilliseconds = Date.prototype.valueOf.call(this);
+		if (isNaN(epochMilliseconds)) {
+			throw new RangeError();
+		}
+		return createTemporalInstant(createEpochNanosecondsFromEpochMilliseconds(epochMilliseconds));
 	}
-	return createTemporalInstant(createEpochNanosecondsFromEpochMilliseconds(epochMilliseconds));
 }
+
+export const toTemporalInstant = TmpClass.prototype.toTemporalInstant;
