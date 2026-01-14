3e782bb9467c1a6b178eb3db93e010b5e2f375a2
fabon <syobon.hinata.public@gmail.com>
2026-01-15 00:14:14 +0900

    Tweak test262 runner script

--- script/test262.ts
+++ script/test262.ts
@@ -1,6 +1,15 @@
 import { mkdir, rm, writeFile } from "node:fs/promises";
+import { parseArgs } from "node:util";
 import runTest262 from "@js-temporal/temporal-test262-runner";
 import { bundle } from "./shared/build.ts";
+
+const { values, positionals: files } = parseArgs({
+	args: process.argv.slice(2),
+	options: {
+		update: { type: "boolean", short: "u" },
+	},
+	allowPositionals: true,
+});
 
 await rm("dist", { recursive: true });
 await mkdir("dist", {});
@@ -9,10 +18,15 @@ await writeFile("dist/bundle.js", await bundle());
 const result = await runTest262({
 	test262Dir: "test262",
 	polyfillCodeFile: "dist/bundle.js",
-	testGlobs: [
-		"test262/test/built-ins/Temporal/**/*.js",
-		"test262/test/built-ins/Date/prototype/toTemporalInstant/*.js",
-	],
+	testGlobs:
+		files.length === 0
+			? [
+					"test262/test/built-ins/Temporal/**/*.js",
+					"test262/test/built-ins/Date/prototype/toTemporalInstant/*.js",
+				]
+			: files,
+	expectedFailureFiles: ["expected-failures.txt"],
+	updateExpectedFailureFiles: values.update,
 });
 
 // if result is `true`, all tests succeeded
