7476900a7c246d2eef4b33757fba1d81d585b94e
fabon <syobon.hinata.public@gmail.com>
2026-01-19 07:52:47 +0900

    Improve display of Temporal objects in REPL script

--- script/repl.ts
+++ script/repl.ts
@@ -1,4 +1,25 @@
 import repl from "node:repl";
 import "../src/global.ts";
+import { inspect } from "node:util";
+import type { Temporal as TemporalNamespace } from "../src/Temporal.ts";
+
+declare var Temporal: typeof TemporalNamespace;
+
+for (const Class of [
+	Temporal.Instant,
+	Temporal.ZonedDateTime,
+	Temporal.PlainDateTime,
+	Temporal.PlainDate,
+	Temporal.PlainTime,
+	Temporal.PlainYearMonth,
+	Temporal.PlainMonthDay,
+	Temporal.Duration,
+]) {
+	Object.defineProperty(Class.prototype, inspect.custom, {
+		value() {
+			return `${this[Symbol.toStringTag]}: ${this.toString()}`;
+		},
+	});
+}
 
 repl.start({ prompt: "> ", useGlobal: true });
