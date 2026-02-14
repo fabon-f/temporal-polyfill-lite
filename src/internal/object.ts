5a4d8bc4242fc94da219d681fe8e6c3d7e0829d6
fabon <syobon.hinata.public@gmail.com>
2026-02-14 13:47:38 +0900

    Improve type annotation

--- src/internal/object.ts
+++ src/internal/object.ts
@@ -2,11 +2,9 @@ export function isObject(obj: unknown): obj is object {
 	return (typeof obj === "object" || typeof obj === "function") && obj !== null;
 }
 
-export function pickObject<O extends object, K extends keyof O>(object: O, keys: K[]): Pick<O, K>;
-export function pickObject(object: object, keys: string[]) {
-	const result = createNullPrototypeObject({});
+export function pickObject<O extends object, K extends keyof O>(object: O, keys: K[]): Pick<O, K> {
+	const result = createNullPrototypeObject({}) as O;
 	for (const key of keys) {
-		// @ts-expect-error
 		result[key] = object[key];
 	}
 	return result;
