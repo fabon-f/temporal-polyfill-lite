33ec6e55f47952bbce9032313d03b64dfeaf8a72
fabon <syobon.hinata.public@gmail.com>
2026-05-28 12:38:52 +0900

    Support Node.js v26 in test262 script

--- script/test262.ts
+++ script/test262.ts
@@ -4,6 +4,15 @@ import runTest262 from "@js-temporal/temporal-test262-runner";
 import { bundle } from "./shared/build.ts";
 
 const isBun = process.versions["bun"] !== undefined;
+const isNode = !isBun;
+
+function parseSemver(version: string): [major: number, minor: number, patch: number] {
+	const match = version.match(/(\d+)\.(\d+)\.(\d+)/);
+	if (!match) {
+		throw new Error(`invalid version: ${version}`);
+	}
+	return [match[1]!, match[2]!, match[3]!].map((v) => parseInt(v)) as [number, number, number];
+}
 
 function expectedFailureFiles(mode: "basic" | "full") {
 	const files = ["ecma262.txt"];
@@ -15,6 +24,12 @@ function expectedFailureFiles(mode: "basic" | "full") {
 	} else {
 		console.log("testing full mode in Bun is not supported for now");
 		process.exit(0);
+	}
+	if (isNode) {
+		const major = parseSemver(process.versions.node)[0];
+		if (major < 26) {
+			files.push("ecma402-node-lt26.txt");
+		}
 	}
 	return files.map((file) => `tests/expectedFailures/${file}`);
 }
