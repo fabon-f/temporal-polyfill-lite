428cfe29e7beb499323160186ede2050d29b2309
fabon <syobon.hinata.public@gmail.com>
2026-06-21 01:37:26 +0900

    Remove unused code

--- src/internal/object.ts
+++ src/internal/object.ts
@@ -2,14 +2,6 @@ export function isObject(obj: unknown): obj is object {
 	return (typeof obj === "object" || typeof obj === "function") && obj !== null;
 }
 
-export function pickObject<O extends object, K extends keyof O>(object: O, keys: K[]): Pick<O, K> {
-	const result = createNullPrototypeObject() as O;
-	for (const key of keys) {
-		result[key] = object[key];
-	}
-	return result;
-}
-
 export function createNullPrototypeObject<O = {}>(obj?: O): O {
 	return Object.assign(Object.create(null), obj);
 }
