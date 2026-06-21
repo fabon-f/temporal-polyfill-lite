cd52bc3ceccb006a3e0db4c00345204c08ec8360
fabon <syobon.hinata.public@gmail.com>
2026-06-21 15:31:13 +0900

    Improve test

--- src/internal/nativeTemporal.test.ts
+++ src/internal/nativeTemporal.test.ts
@@ -4,15 +4,17 @@ import { createNativeZonedDateTime } from "./nativeTemporal.ts";
 describe("createNativeZonedDateTime", () => {
 	test("basic", () => {
 		expect(createNativeZonedDateTime(0, "America/Los_Angeles")).toEqual(
-			new Temporal.ZonedDateTime(0n, "America/Los_Angeles"),
+			Temporal.Instant.fromEpochMilliseconds(0).toZonedDateTimeISO("America/Los_Angeles"),
 		);
 	});
 	test.for([8.64e15, -8.64e15])("extreme instant: %i", (epochMilliseconds) => {
 		expect(createNativeZonedDateTime(epochMilliseconds, "America/Los_Angeles")).toEqual(
-			new Temporal.ZonedDateTime(BigInt(epochMilliseconds) * 1000000n, "America/Los_Angeles"),
+			Temporal.Instant.fromEpochMilliseconds(epochMilliseconds).toZonedDateTimeISO(
+				"America/Los_Angeles",
+			),
 		);
 		expect(createNativeZonedDateTime(epochMilliseconds, "Asia/Tokyo")).toEqual(
-			new Temporal.ZonedDateTime(BigInt(epochMilliseconds) * 1000000n, "Asia/Tokyo"),
+			Temporal.Instant.fromEpochMilliseconds(epochMilliseconds).toZonedDateTimeISO("Asia/Tokyo"),
 		);
 	});
 	test("invalid time zone", () => {
