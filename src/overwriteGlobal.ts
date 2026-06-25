5924f274aeeab30178dc053ec6a6c7ee5ca797e6
fabon <syobon.hinata.public@gmail.com>
2026-06-25 12:49:17 +0900

    Enable test262 check in Node.js v26

--- src/overwriteGlobal.ts
+++ src/overwriteGlobal.ts
@@ -1,4 +1,22 @@
-import { install } from "./shim.ts";
+import { Intl, Temporal, toTemporalInstant } from "./index.ts";
 
 // Always overwrite the existing Temporal implementation for test262 and REPL
-install(true);
+// FIXME: Replace workaround with `install(true)` after https://github.com/nodejs/node/issues/64008 is fixed
+Object.defineProperty(globalThis, "Temporal", {
+	value: Temporal,
+	writable: true,
+	enumerable: false,
+	configurable: true,
+});
+Object.defineProperty(globalThis, "Intl", {
+	value: Intl,
+	writable: true,
+	enumerable: false,
+	configurable: true,
+});
+Object.defineProperty(Date.prototype, "toTemporalInstant", {
+	value: toTemporalInstant,
+	writable: true,
+	enumerable: false,
+	configurable: true,
+});
