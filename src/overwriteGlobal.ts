08e2045e4979de9fd9cefc8978bc1225452db8fb
fabon <syobon.hinata.public@gmail.com>
2026-07-11 11:45:30 +0900

    Remove workaround for Node.js v26

--- src/overwriteGlobal.ts
+++ src/overwriteGlobal.ts
@@ -1,22 +1,4 @@
-import { Intl, Temporal, toTemporalInstant } from "./index.ts";
+import { install } from "./shim.ts";
 
 // Always overwrite the existing Temporal implementation for test262 and REPL
-// FIXME: Replace workaround with `install(true)` after https://github.com/nodejs/node/issues/64008 is fixed
-Object.defineProperty(globalThis, "Temporal", {
-	value: Temporal,
-	writable: true,
-	enumerable: false,
-	configurable: true,
-});
-Object.defineProperty(globalThis, "Intl", {
-	value: Intl,
-	writable: true,
-	enumerable: false,
-	configurable: true,
-});
-Object.defineProperty(Date.prototype, "toTemporalInstant", {
-	value: toTemporalInstant,
-	writable: true,
-	enumerable: false,
-	configurable: true,
-});
+install(true);
